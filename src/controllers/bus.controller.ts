bf804859beb83e9d4cbecfe68945cb55e93ba6bb
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-04-01 01:13:46 +0100

    updated

--- src/controllers/bus.controller.ts
+++ src/controllers/bus.controller.ts
@@ -4,6 +4,8 @@ import { Response, Request } from 'express';
 import busModel from '../models/bus.model';
 import { isThisTypeNode } from 'typescript';
 import mongoose from 'mongoose';
+import { IBus } from '../@types/models';
+import seatModel from '../models/seat.model';
 
 export const handleListBuses = async (req: Request, res: Response) => {	 
   const buses = await busModel.find({}).populate(["seats", {
@@ -32,4 +34,35 @@ export const handleRouteListBuses = async (req: Request<{id: string}>, res: Resp
   }])
 
   res.status(200).send(response("Route buses", buses))
+}
+
+export const handleUpdateBus = async (req: Request<{id: string}>, res: Response) => {
+  if(!req.params.id) throw new BadRequestError("Route ID is required")	 
+
+  // Check if bus exists
+  const busExists = await busModel.findOne({ _id: req.params.id })
+  if(!busExists) throw new NotFoundError("Bus not found")
+
+  await busExists.updateOne({...req.body})
+
+  if(req.body.seatNumber && req.body.seatNumber !== busExists.seats.length) {
+    // DELETE ALL THE OTHER SEATS
+    for (let index = 0; index < busExists.seats.length; index++) {
+      await seatModel.findByIdAndDelete(busExists.seats[index])
+    }
+
+    const seats = []
+    // ADD NEW SEATS
+    for (let index = 1; index <= req.body.seatNumber; index++) {
+      let seat = await seatModel.create({bus: busExists._id, seatNo: index})
+      seats.push(seat._id)
+    }
+  
+    // Update the bus object
+    busExists.seats = seats as any[]
+    await busExists.save()
+  }
+
+  const bus = await busModel.findById(req.params.id)
+  res.status(200).send(response("Bus updated", bus))
 }
--- src/routes/bus.routes.ts
+++ src/routes/bus.routes.ts
@@ -1,5 +1,5 @@
 import { authMiddleware } from '../middlewares/auth.middleware';
-import { handleDeleteBus, handleListBuses, handleRouteListBuses } from './../controllers/bus.controller';
+import { handleDeleteBus, handleListBuses, handleRouteListBuses, handleUpdateBus } from './../controllers/bus.controller';
 import { Router } from 'express';
 import configs from '../configs';
 const { roles } = configs;
@@ -15,6 +15,11 @@ busRoutes.get("/:id", handleRouteListBuses)
 
 // Route Buses
 busRoutes.delete("/:id", authMiddleware(roles.ADMIN) as any, handleDeleteBus)
+busRoutes.get("/:id", handleRouteListBuses)
+
+// Route Buses
+busRoutes.patch("/:id", authMiddleware(roles.ADMIN) as any, handleUpdateBus)
+busRoutes.put("/:id", authMiddleware(roles.ADMIN) as any, handleUpdateBus)
 
 
 
